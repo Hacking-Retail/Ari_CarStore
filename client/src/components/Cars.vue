1099861a86c010eda6228d884f4b066cc974ac3b
littlxri <ariana.aqifi@gmail.com>
2020-08-21 15:56:24 +0200

    filter on maker and color

--- client/src/components/Cars.vue
+++ client/src/components/Cars.vue
@@ -4,7 +4,67 @@
       <div class="col-sm-10">
         <h1>Cars in store</h1>
         <hr><br><br>
+        <label for="car-select">Search by filters</label>
+        <form>
+            <select name="maker" id="maker-select">
+                <option value="">--Maker--</option>
+                <option value="alpha-romeo">Alpha Romeo</option>
+                <option value="audi">Audi</option>
+                <option value="bmw">BMW</option>
+                <option value="chevrolet">Chevrolet</option>
+                <option value="crysler">Crysler</option>
+                <option value="citroen">Citroen</option>
+                <option value="dacia">Dacia</option>
+                <option value="dodge">Dodge</option>
+                <option value="fiat">Fiat</option>
+                <option value="ford">Ford</option>
+                <option value="honda">Honda</option>
+                <option value="hummer">Hummer</option>
+                <option value="hyundai">Hyundai</option>
+                <option value="jaguar">Jaguar</option>
+                <option value="jeep">Jeep</option>
+                <option value="kia">Kia</option>
+                <option value="lancia">Lancia</option>
+                <option value="land-rover">Land Rover</option>
+                <option value="lexus">Lexus</option>
+                <option value="mazda">Mazda</option>
+                <option value="mercedes-benz">Mercedes Benz</option>
+                <option value="mini">Mini</option>
+                <option value="mitsubishi">Mitsubishi</option>
+                <option value="nissan">Nissan</option>
+                <option value="opel">Opel</option>
+                <option value="peugeot">Peugeot</option>
+                <option value="porsche">Porsche</option>
+                <option value="renault">Renault</option>
+                <option value="seat">Seat</option>
+                <option value="skoda">Skoda</option>
+                <option value="smart">Smart</option>
+                <option value="subaru">Subaru</option>
+                <option value="suzuki">Suzuki</option>
+                <option value="toyota">Toyota</option>
+                <option value="volkswagen">Volkswagen</option>
+                <option value="volvo">Volvo</option>
+            </select>
+
+            <select name="color" id="color-select">
+                <option value="">--Color--</option>
+                <option value="beige">Beige</option>
+                <option value="black">Black</option>
+                <option value="blue">Blue</option>
+                <option value="brown">Brown</option>
+                <option value="gold">Gold</option>
+                <option value="green">Green</option>
+                <option value="orange">Orange</option>
+                <option value="red">Red</option>
+                <option value="silver">Silver</option>
+                <option value="violet">Violet</option>
+                <option value="white">White</option>
+                <option value="yellow">Yellow</option>
+            </select>
+        </form>
+        <input type="submit" value="Search" @click="getCars">
         <br><br>
+
         <table class="table table-hover">
           <thead>
             <tr>
@@ -81,7 +141,7 @@ export default {
 
   methods: {
     getCars() {
-      const path = 'http://localhost:5000/api/cars/all';
+      const path = this.filter();
       axios.get(path)
         .then((res) => {
           this.cars = res.data.cars;
@@ -103,7 +163,43 @@ export default {
           // eslint-disable-next-line
           console.error(error);
         });
-    }
+    },
+
+    filter() {
+        var m = document.getElementById("maker-select");
+        var c = document.getElementById("color-select");
+
+        if(m != null) {
+            var maker = m.options[m.selectedIndex].value;
+        }
+        else {
+            var maker = "";
+        }
+
+        if(c != null) {
+            var color = c.options[c.selectedIndex].value;
+        }
+        else {
+            var color = "";
+        }
+
+        console.log("maker : " + maker)
+        console.log("color : " + color)
+        if(maker != "" && color != "") {
+            return  `http://localhost:5000/api/cars?maker=${maker}&color_slug=${color}`
+        }
+        else if(maker != "" && color == "") {
+            return  `http://localhost:5000/api/cars?maker=${maker}`
+        }
+        else if(maker == "" && color != "") { 
+            return  `http://localhost:5000/api/cars?&color_slug=${color}`
+        }
+        else {
+            return "http://localhost:5000/api/cars/all"
+        }
+
+  }
+
   },
   
   created() {
